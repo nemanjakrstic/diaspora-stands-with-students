6cc9c28abe701314be058ca1d2e518a11a765828
Nemanja <nemanja.d.krstic@gmail.com>
2025-02-09 15:25:06 -0800

    Instagram link

--- src/messages.ts
+++ src/messages.ts
@@ -3,6 +3,8 @@ const en = {
   title: "Diaspora Stands with Students",
   filter: "Find place",
   click_to_show_support: "Click to show support",
+  follow_our_instagram: "Follow our Instagram profile",
+  project_information: "Project information",
 };
 
 export type Messages = typeof en;
@@ -12,6 +14,8 @@ const sr: Messages = {
   title: "Dijaspora uz studente",
   filter: "Pronađi mesto",
   click_to_show_support: "Klikni da pokažeš podršku",
+  follow_our_instagram: "Prati naš Instagram profil",
+  project_information: "Informacije o projektu",
 };
 
 export const messages = {
