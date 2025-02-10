2c3b10cfa7e10a85f007c51f102a42dac562ca21
Nemanja Krstic <nemakrst@amazon.com>
2025-02-10 12:24:24 -0800

    Refresh recaptcha token on reconnect

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,8 +1,10 @@
 import { socket } from "./socket";
 
-grecaptcha.ready(function () {
-  grecaptcha.execute(import.meta.env.VITE_RECAPTCHA_SITE_KEY, { action: "submit" }).then((token: string) => {
-    console.log("Setting up reCAPTCHA...");
-    socket.emit("init", { token });
+socket.on("connect", () => {
+  grecaptcha.ready(function () {
+    grecaptcha.execute(import.meta.env.VITE_RECAPTCHA_SITE_KEY, { action: "submit" }).then((token: string) => {
+      console.log("Setting up reCAPTCHA...");
+      socket.emit("init", { token });
+    });
   });
 });
