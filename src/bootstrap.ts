35afb39e3478004514cd92a6a660b78d50c19683
Nemanja <nemanja.d.krstic@gmail.com>
2025-02-09 21:10:06 -0800

    Recaptcha fixes

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,16 +1,8 @@
 import { socket } from "./socket";
 
-// grecaptcha.ready(function () {
-//   grecaptcha.execute(import.meta.env.VITE_RECAPTCHA_SITE_KEY, { action: "submit" }).then((token: string) => {
-//     socket.emit("init", { token });
-//   });
-// });
-
-interface Window {
-  onSubmit?: (token: string) => void;
-}
-
-(window as unknown as Window).onSubmit = function (token: string) {
-  console.log({ token });
-  socket.emit("init", { token });
-};
+grecaptcha.ready(function () {
+  grecaptcha.execute(import.meta.env.VITE_RECAPTCHA_SITE_KEY, { action: "submit" }).then((token: string) => {
+    console.log("Setting up reCAPTCHA...");
+    socket.emit("init", { token });
+  });
+});
