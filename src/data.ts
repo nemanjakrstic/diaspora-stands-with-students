4544f1db7767b33a683ca97d09866747ada1c275
Nemanja <nemanja.d.krstic@gmail.com>
2025-02-11 21:26:36 -0800

    Sort event date badges

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-import { groupBy, map } from "lodash";
+import { groupBy, map, orderBy } from "lodash";
 import csv from "./data.csv?raw";
 
 const rows = csv
@@ -7,12 +7,15 @@ const rows = csv
   .map((line) => {
     const [city, country, date, location, url] = line.split("\t");
     const [lat, lng] = location.split(",");
+    const [day, month, year] = date ? date.split(".") : [];
+    const dateIso = date ? [year, month, day].join("-") : null;
 
     return {
       title: [city, country].filter(Boolean).join(", "),
       city: city || undefined,
       country,
       date: date || "N/A",
+      dateIso,
       location: { lng: parseFloat(lng), lat: parseFloat(lat) },
       url,
     };
@@ -38,7 +41,7 @@ export const places: Place[] = map(
     city: events[0].city,
     country: events[0].country,
     location: events[0].location,
-    events: events.map((event) => ({ date: event.date, url: event.url })),
+    events: orderBy(events, (event) => event.dateIso, "desc").map((event) => ({ date: event.date, url: event.url })),
   }),
 );
 
